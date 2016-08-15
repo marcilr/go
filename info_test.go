5e29259d8d257188a2a3326cdafe5a4d65760bb8
Raymond E. Marcil <marcilr@gmail.com>
2016-08-15 14:40:40 -0800

    info_test.go   Package to test info used to display messages.
      Package to show use of info() function to display debugging
      output.  info() used to be naming debug() but info() is a
      bit more manager friendly.

---
+++ info_test.go
@@ -0,0 +1,25 @@
+/*
+   info.go
+   Created Mon Aug 15 14:32:11 AKDT 2016
+   Copyright (C) 2016 by Raymond E. Marcil <marcilr@gmail.com>
+
+   Package to test info used to display messages.
+
+   Package to show use of info() function to display debugging
+   output.  info() used to be naming debug() but info() is a 
+   bit more manager friendly.
+*/
+package main
+
+import "testing"
+import "fmt"
+
+/*
+ * TestRead()
+ * Test reading ini file.
+ */
+func TestInfo(t *testing.T) {
+
+  Info("Test Info() successful")
+
+}   // End TestInfo
