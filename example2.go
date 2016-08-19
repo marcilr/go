6d19d6bf7cf5978390875edfce8366ed21ad4f87
Raymond E. Marcil <marcilr@gmail.com>
2016-08-19 10:44:30 -0800

    example2.go    Example of using Go MySQL Driver to create database handle and execute query against table.

---
+++ example2.go
@@ -0,0 +1,18 @@
+/*
+   example2.go
+   Created Fri Aug 19 10:39:24 AKDT 2016
+   by Raymond E. Marcil <marcilr@gmail.com>
+
+   Example of using Go MySQL Driver to create database handle and execute query against table.
+
+*/
+package main
+
+
+/*
+ * Main()
+ */
+func Main() {
+
+
+} // End Main()
