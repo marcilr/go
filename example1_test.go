39be0bbee9522e1607aa8b6405ecbba63d2e748b
Raymond E. Marcil <marcilr@gmail.com>
2016-08-19 10:04:48 -0800

    exampl1_test.go   Testing package for tutorial.go

---
+++ example1_test.go
@@ -0,0 +1,23 @@
+/*
+   example1_test.go
+   Created Thu Aug 18 10:25:16 AKDT 2016
+   by Raymond E. Marcil <marcilr@gmail.com>
+
+   Testing package for exampl1.go
+*/
+package main
+
+import "testing"
+
+/*
+ * TestFoo()
+ */
+func TestFoo(t *testing.T) {
+} // End TestFoo()
+
+/*
+ * TesMain()
+ */
+func TestMain(t *testing.T) {
+    Main()
+} // End TestFoo()
